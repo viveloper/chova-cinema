2cbcd78fb1c845cd13f241eeaeaf2e018e87027b
viveloper <ucsik1234@gmail.com>
2023-02-27 17:05:11 +0900

    fix: change server port

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.get('*', (req, res) =>
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 );
 
-const PORT = 5500;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
